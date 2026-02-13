be003e7176e848a93fd66b63bff3198daae8be77
soseoyo12 <songsy0612@gmail.com>
2026-02-14 01:45:11 +0900

    #20 manage-skills 및 verify-implementation 스킬 추가

---
+++ Bobmoo_iOS/Bobmoo_iOS/Components/BobmooTextField.swift
@@ -0,0 +1,18 @@
+//
+//  BobmooTextField.swift
+//  Bobmoo_iOS
+//
+//  Created by 송성용 on 2/14/26.
+//
+
+import SwiftUI
+
+struct BobmooTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BobmooTextField()
+}
