2fee8a8c86016f0f696e6c9f3170438390766c86
soseoyo12 <songsy0612@gmail.com>
2026-02-27 17:06:12 +0900

    fix: 위젯 content margin 제거로 배치 보정

--- Bobmoo_iOS/BobmooWidgetExtension/BobmooWidgetExtension.swift
+++ Bobmoo_iOS/BobmooWidgetExtension/BobmooWidgetExtension.swift
@@ -7,8 +7,12 @@ struct DietWidget: Widget {
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: DietProvider()) { entry in
             DietWidgetView(entry: entry)
-                .widgetURL(URL(string: "bobmoo://home"))
+                .widgetURL({
+                    let scheme = Bundle.main.object(forInfoDictionaryKey: "APP_URL_SCHEME") as? String ?? "bobmoo"
+                    return URL(string: "\(scheme)://home")
+                }())
         }
+        .contentMarginsDisabled()
         .configurationDisplayName("Bobmoo Widget")
         .description("학식 정보를 보여주는 위젯입니다.")
         .supportedFamilies([.systemSmall, .systemMedium])
